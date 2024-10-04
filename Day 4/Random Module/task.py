edad5ecadd134e9594dc7116e2e8b260ca87439c
Allan Oliveira <allanoliveira.dev@gmail.com>
2024-10-04 14:54:30 -0300

    create heads or tails logic

--- Day 4/Random Module/task.py
+++ Day 4/Random Module/task.py
@@ -1,6 +1,17 @@
 import random
+from math import floor
+
 from my_module import my_fav_num
 
-random_int = random.randint(1, 10)
+# random_int = random.randint(1, 10)
 
-print(my_fav_num)
+# random_num_0_to_1 = floor(random.random() * 11)
+#
+# rand_float = random.uniform(1, 10)
+
+num = (random.randint(1,2))
+
+if num == 1:
+    print("heads")
+else:
+    print("tails")
