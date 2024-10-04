8a736cc29e2cec74f68cd1bda8725231683c7deb
Allan Oliveira <allanoliveira.dev@gmail.com>
2024-10-04 00:42:43 -0300

    format individual amount

--- Day 2/Tip Calculator Project/task.py
+++ Day 2/Tip Calculator Project/task.py
@@ -4,6 +4,6 @@ tip = int(input("What percentage tip would you like to give? 10 12 15 "))
 people = int(input("How many people to split the bill? "))
 tip_percentage = tip / 100
 total_bill = bill + (bill * tip_percentage)
-
-# print(round(total_bill, 2))
-
+each_person_amount = round(total_bill / people, 2)
+formated_individual_amount = "%0.2f" % each_person_amount
+print(f"Each person should pay ${formated_individual_amount}")
