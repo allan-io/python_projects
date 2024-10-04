cf27f00cad6f0239892d0db09ee978831c21c73e
Allan Oliveira <allanoliveira.dev@gmail.com>
2024-10-04 00:22:04 -0300

    calculate tip percentage

--- Day 2/Tip Calculator Project/task.py
+++ Day 2/Tip Calculator Project/task.py
@@ -2,5 +2,8 @@ print("Welcome to the tip calculator!")
 bill = float(input("What was the total bill? $"))
 tip = int(input("What percentage tip would you like to give? 10 12 15 "))
 people = int(input("How many people to split the bill? "))
+tip_percentage = tip / 100
+# total_bill = bill + (bill * tip_percentage)
 
+# print(round(total_bill, 2))
 
