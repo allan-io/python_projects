6ff0acee222c89e128cc1eaf0b15d3b99390065b
Allan Oliveira <allanoliveira.dev@gmail.com>
2024-10-04 00:22:53 -0300

    calculate total bill

--- Day 2/Tip Calculator Project/task.py
+++ Day 2/Tip Calculator Project/task.py
@@ -3,7 +3,7 @@ bill = float(input("What was the total bill? $"))
 tip = int(input("What percentage tip would you like to give? 10 12 15 "))
 people = int(input("How many people to split the bill? "))
 tip_percentage = tip / 100
-# total_bill = bill + (bill * tip_percentage)
+total_bill = bill + (bill * tip_percentage)
 
 # print(round(total_bill, 2))
 
