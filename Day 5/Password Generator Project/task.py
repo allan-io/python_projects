b0facae66719c05cbeb29bf35af29c929a1ff9e7
Allan Oliveira <allanoliveira.dev@gmail.com>
2024-10-05 12:39:33 -0300

    create password generator

--- Day 5/Password Generator Project/task.py
+++ Day 5/Password Generator Project/task.py
@@ -1,3 +1,5 @@
+import random
+
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -7,4 +9,25 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+password_list = []
 
+for num in range(1, nr_letters + 1):
+    random_index = random.randint(1, len(letters) - 1)
+    password_list += letters[random_index]
+
+for num in range(1, nr_numbers + 1):
+    random_index = random.randint(1, len(numbers) - 1)
+    password_list += numbers[random_index]
+
+for num in range(1, nr_symbols + 1):
+    random_index = random.randint(1, len(symbols) - 1)
+    password_list += symbols[random_index]
+
+random.shuffle(password_list)
+
+password = ""
+
+for item in password_list:
+    password += item
+
+print(f"Your password is: {password}")
