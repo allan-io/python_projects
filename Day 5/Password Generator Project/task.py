6d37841740ee4e3353bb1218e3592e462f0d02e0
Allan Oliveira <allanoliveira.dev@gmail.com>
2024-10-05 12:49:13 -0300

    remove user input and automate password creation

--- Day 5/Password Generator Project/task.py
+++ Day 5/Password Generator Project/task.py
@@ -5,9 +5,9 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-nr_letters = int(input("How many letters would you like in your password?\n"))
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+nr_letters = random.randint(7, 12)
+nr_symbols = random.randint(7, 12)
+nr_numbers = random.randint(7, 12)
 
 password_list = []
 
