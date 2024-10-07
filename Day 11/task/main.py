50a8293109327457aa6b0f2922975fc8b822c3f5
Allan Oliveira <allanoliveira.dev@gmail.com>
2024-10-07 13:24:25 -0300

    create blackjack logic

--- Day 11/task/main.py
+++ Day 11/task/main.py
@@ -6,44 +6,87 @@ start_game_input = input("Do you want to play a game of blackjack? type 'y' or '
 # after round is complete program should check with user again
 start_game = utils.start_game(start_game_input)
 
-def display_current_score():
-    print("Current Score")
+def play_again():
+    inp = input("Would you like to play another game of blackjack? Type 'y' for yes, or 'n' for no.")
+    if inp:
+        run_blackjack()
 
-def display_comp_first_card():
-    print("Computer First Card")
+#check if comp has blackjack after player enters 'n' and wa
 
-def display_comp_rest_cards():
-    print("Rest of computers cards")
+    # if the start_game var is True, all game logic should happen until conclusion
 
-def check_if_player_bust():
-    print("player bust")
-#
-def check_who_wins():
-    print("Who wins")
+def run_blackjack():
 
-def hit_or_pass():
-    inp = input("Type 'y' to get another card, type 'n' to pass: ")
-    return inp
-
-# if the start_game var is True, all game logic should happen until conclusion
-if start_game:
     # while game is not over, player should be able to choose another card
     # game is over when player busts or does not want another card
     game_over = False
     print(art.logo)
-    while not game_over:
-        player_init_cards_and_score = utils.init_player_cards_and_score()
-        player_init_cards = player_init_cards_and_score[0]
-        player_init_score = player_init_cards_and_score[1]
-        print(f"Your cards: {player_init_cards}, current score: {player_init_score}")
-        # display the computers first card
-        # keep track of player cards and scores and computer cards and scores
-        another_card_prompt = hit_or_pass()
-        #if "y", get another card from the cards list check if score is <= 21
-        # if <= 21 add another card to card display and add score
-        # else if score is over 21 check if any 11s to use as 1 instead A = 11 or 1
-        # if no 11 to change to 1 game_over = True, you lose
-        # else if "n" run function display_comp_rest_cards this also calcs comp score
-        # after we should run check_who_wins
-        #display who wins and why "opponent went over", "you went over", and always
-        # your final hand and your final score, computers final hand, computer final score
+
+    player_init_cards_and_score = utils.init_player_cards_and_score()
+    player_init_cards = player_init_cards_and_score[0]
+    player_init_score = player_init_cards_and_score[1]
+    comp_init_score = utils.display_comp_first_card()
+
+    print(f"Your cards: {player_init_cards}, current score: {player_init_score}")
+    print(f"Computer's first card: {comp_init_score}")
+
+    player_current_cards = player_init_cards
+    player_current_score = player_init_score
+    comp_current_score = comp_init_score
+    comp_current_cards = [comp_init_score]
+    start_game = utils.check_if_blackjack(player_init_score)
+
+    if not start_game:
+        print("You have blackjack")
+        restart_input = input("Do you want to play a game of blackjack? type 'y' or 'no': ").lower()
+        restart_game = utils.start_game(restart_input)
+        if restart_game:
+            run_blackjack()
+
+
+
+    while not game_over and start_game:
+
+        bust = False
+
+        another_card_prompt = utils.hit_or_pass()
+        if not another_card_prompt:
+            comp_final_cards_and_score = utils.display_comp_rest_cards(comp_init_score)
+            comp_current_cards = comp_final_cards_and_score[0]
+            comp_current_score = comp_final_cards_and_score[1]
+            comp_has_11 = 11 in comp_current_cards
+            if comp_has_11:
+                if comp_current_score == 21:
+                    game_over = True
+                    print("Computer has blackjack 💩")
+                result = utils.change_11_to_1(comp_current_cards)
+                comp_current_cards = result[0]
+                comp_current_score = result[1]
+            print(comp_current_score)
+            utils.check_who_wins(comp_current_score, player_current_score)
+            game_over = True
+        if not game_over:
+            new_card = utils.get_another_card()
+            player_current_cards.append(new_card)
+            player_current_score += new_card
+            bust = utils.check_if_player_bust(player_current_score)
+        if bust:
+            player_has_11 = 11 in player_current_cards
+            if player_has_11:
+                result = utils.change_11_to_1(player_current_cards)
+                player_current_cards = result[0]
+                player_current_score = result[1]
+            else:
+                # run_blackjack()
+                game_over = True
+                print("You Lose")
+                print(f"Computer's final hand: {comp_current_cards}, final score: {comp_current_score}")
+                play_again()
+        # change this around so you lose is the last thing you see
+        utils.display_current_cards_and_score(player_current_cards, player_current_score)
+
+    # after we should run check_who_wins
+    #display who wins and why "opponent went over", "you went over", and always
+    # your final hand and your final score, computers final hand, computer final score
+if start_game:
+    run_blackjack()
--- Day 11/task/utils.py
+++ Day 11/task/utils.py
@@ -1,4 +1,5 @@
 import random
+
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 def start_game(usr_input):
@@ -12,4 +13,68 @@ def init_player_cards_and_score():
     init_player_cards = [random.choice(cards), random.choice(cards)]
     init_player_score = sum(init_player_cards)
     return [init_player_cards, init_player_score]
+
+
+def display_comp_first_card():
+    return random.choice(cards)
+
+def hit_or_pass():
+    inp = input("Type 'y' to get another card, type 'n' to pass: ").lower()
+    if inp == "n":
+        return False
+    else:
+        return True
+
+def get_another_card():
+    return random.choice(cards)
+
+def display_current_cards_and_score(cards_list, score):
+    print(f"Your cards: {cards_list}, current score: {score}")
+
+def check_if_player_bust(score):
+    if score > 21:
+        return True
+    else:
+        return False
+
+def change_11_to_1(player_cards):
+    new_cards = []
+    new_score = 0
+    for card in player_cards:
+        if card == 11:
+            new_score += 1
+            new_cards.append(1)
+        else:
+            new_cards.append(card)
+            new_score += card
+    return [new_cards, new_score]
+
+
+def display_comp_rest_cards(comp_card):
+    comp_final_cards = [comp_card]
+    new_card = random.choice(cards)
+    final_score = False
+
+    while not final_score:
+        if sum(comp_final_cards) < 17:
+            comp_final_cards.append(new_card)
+        else:
+            final_score = True
+
+    print(f"Computer final hand: {comp_final_cards}, final score: {sum(comp_final_cards)}")
+    return [comp_final_cards, sum(comp_final_cards)]
+
+def check_who_wins(comp_score, player_score):
+    if not comp_score > 21 and comp_score > player_score:
+        print("You Lose 😤")
+    elif comp_score == player_score:
+        print("It's a draw 😶")
+    else:
+        print("You win! 😁")
+
+def check_if_blackjack(player_score):
+    if player_score == 21:
+        return False
+    else:
+        return True
 
