17373423925bfbf5ed6e18cccd879c03a8fead11
Allan Oliveira <allanoliveira.dev@gmail.com>
2024-10-09 02:30:15 -0300

    initial commit

---
+++ Day 14/Higher or Lower Project/main.py
@@ -0,0 +1,24 @@
+from game_data import data
+import art
+
+def play_game():
+    print(art.logo)
+
+
+play_game()
+#define play_game function and call function to start game
+
+#import art and gamedata
+# choose 2 entries from the dictionary and save to vars profile_a and profile_b
+#create game over variable set to False. Turn this to true once player gets answer wrong
+#create score variable that changes with each correct guess until game over
+#in a while loop check for game over if game not over AKA player has not gotten answer wrong:
+    #print description about each profile with VS art in between
+    #ask player to choose one
+    #if correct, profile b becomes profile a and we get a new random profile from gamedata
+    # add one to score and display score
+    #if wrong answer, display score
+
+
+
+
